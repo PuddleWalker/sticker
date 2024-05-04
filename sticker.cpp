a8bf6b4593c62746a552e1f7665d71b152068092
Тимур Миндубаев <mindubaev.timurka@gmail.com>
2024-05-04 20:33:01 +0300

    Добавьте файлы проекта.

---
+++ sticker.cpp
@@ -0,0 +1,17 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	vector<int> a(10);
+
+	for (auto tt : a)
+	{
+		int i = 0;
+		tt = i * i;
+		cout << tt << " ";
+		i++;
+	}
+}
